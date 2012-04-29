f79125613a2022e3aad0921aa64f4017e869e6ab
Adrian Stratulat <adrian.stratulat@cti.pub.ro>
2012-04-29 18:02:49 +0300

    Update mockup data

--- db/migrate/50_add_mockup_user.rb
+++ db/migrate/50_add_mockup_user.rb
@@ -1,4 +1,6 @@
 class AddMockupUser < ActiveRecord::Migration
-	User.new(:email => "adrian.stratulat@cti.pub.ro", :password => "p").save()
-	User.new(:email => "still_me@internetz", :password => "p").save()
+	User.new(:email => "adrian.stratulat@cti.pub.ro",
+		     :password => "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3").save()
+	User.new(:email => "still_me@internetz",
+			 :password => "p").save()
 end
--- db/migrate/51_add_mockup_serie.rb
+++ db/migrate/51_add_mockup_serie.rb
@@ -1,16 +1,21 @@
 class AddMockupSerie < ActiveRecord::Migration
 	Serie.new(:name => "Doctor Who",
-				:show_id => 0,
+				:show_id => 3332,
 				:clasifiscat => "awesome",
 				:url => "blahblah").save()
 
 	Serie.new(:name => "Fringe",
-				:show_id => 1,
+				:show_id => 18388,
 				:clasifiscat => "awesome",
 				:url => "...").save()
 
 	Serie.new(:name => "Glee",
-				:show_id => 7,
+				:show_id => 21704,
 				:clasifiscat => "not awesome",
 				:url => "...").save()
+
+	Serie.new(:name => "Californication",
+				:show_id => 15319,
+				:clasifiscat => "awesome",
+				:url => "...").save()	
 end
